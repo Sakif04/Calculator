0f2b28910a7fb9443f2c35001223ad9a597dc1e8
Sakif04 <sakifrafid04@gmail.com>
2021-09-19 22:29:35 +0600

    completed logic

--- js/script.js
+++ js/script.js
@@ -10,7 +10,16 @@ const numberBtn = [
     "nine",
     "zero",
 ];
-const executionBtn = ["add", "multiply", "subtract", "divide", "percentage"];
+const executionId = {
+    add: "add-btn",
+    mul: "multiply-btn",
+    sub: "subtract-btn",
+    div: "divide-btn",
+    percent: "percent-btn",
+    divide: "divide",
+    equal: "equal-btn",
+    reset: "reset-btn",
+};
 
 function selector(element) {
     return document.querySelector(element);
@@ -28,3 +37,72 @@ numberBtn.forEach((btn) => {
         input.value += e.target.innerText;
     });
 });
+let initialFunc;
+let output;
+function execute(e) {
+    try {
+        let inputNum = parseFloat(input.value);
+        let isInputcorrect = typeof input.value;
+        console.log(e.target.id);
+        console.log(isInputcorrect);
+        const clickedBtnId = Object.values(executionId).find(
+            (id) => e.target.id === id
+        );
+
+        switch (clickedBtnId) {
+            case executionId.add:
+                initialFunc = add(inputNum);
+                console.log(initialFunc);
+                input.value = "";
+                break;
+            case executionId.div || executionId.divide:
+                initialFunc = divide(inputNum);
+                input.value = "";
+                break;
+            case executionId.sub:
+                initialFunc = subtract(inputNum);
+                input.value = "";
+                break;
+            case executionId.mul:
+                initialFunc = multiply(inputNum);
+                input.value = "";
+                break;
+            case executionId.percent:
+                output = percent(inputNum);
+                break;
+            case executionId.reset:
+                output = "";
+                break;
+            case executionId.equal:
+                output = initialFunc(inputNum);
+                break;
+            case "default":
+                break;
+        }
+        input.value = output;
+    } catch (err) {
+        alert(err.message);
+    }
+}
+console.log();
+Object.values(executionId).forEach((btn) => {
+    const btnId = selector("#" + btn);
+    click(btnId, execute);
+});
+
+const add = (a) => {
+    return (b) => a + b;
+};
+const subtract = (a) => {
+    return (b) => a - b;
+};
+const multiply = (a) => {
+    return (b) => a * b;
+};
+const percent = (a) => {
+    let x = a < 1 ? a * 100 : "";
+    return x;
+};
+const divide = (a) => {
+    return (b) => a / b;
+};
